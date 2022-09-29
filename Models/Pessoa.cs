185a04a879108201146ba796af726c9e52f630b8
filipesenaifirjan <filipemattossenai@gmail.com>
2022-09-29 11:57:10 -0300

    ex de uma classe

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -5,14 +5,14 @@ using System.Threading.Tasks;
 
 namespace sintaxe_em_c_.Models
 {
-    public class Pessoa
+    public class Pessoa //uma classe, nada mais é que um molde que vc quer representar
     {
         public string Nome {get; set; }
-        public int MyProperty { get; set; }
+        public int Idade { get; set; }
 
         public void Apresentar()
         {
-            Console.WriteLine($ "Olá, meu nome é {Nome}, e tenho  {Idade} anos");
+            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho  {Idade} anos");
         }
     }
 }
