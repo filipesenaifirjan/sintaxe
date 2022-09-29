eb4701a6092a2c232ec57a5c17ec209999469a92
filipesenaifirjan <filipemattossenai@gmail.com>
2022-09-29 11:06:38 -0300

    2

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -7,6 +7,12 @@ namespace sintaxe_em_c_.Models
 {
     public class Pessoa
     {
+        public string Nome {get; set; }
         public int MyProperty { get; set; }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($ "Olá, meu nome é {Nome}, e tenho  {Idade} anos");
+        }
     }
 }
