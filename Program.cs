4747f2f42d634616cc9706611306b5da06e65113
filipesenaifirjan <filipemattossenai@gmail.com>
2022-09-30 12:31:42 -0300

    4.2

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using sintaxe_em_c_.Models; //aqui se trata de um caminho (lógico) que não é referenciado em pastas (não precisa)
 //Obs: Se a sua classe não estiver sendo reconhecida, presta atenção no namespace
 
-Pessoa pessoa1 = new Pessoa(); //a pessoa1 pode levar qualquer nome, ali é o nome da variável, vc pode modificar, pois se trata de uma instância
+Pessoa pessoa1 = new Pessoa(); //a pessoa1 pode levar qualquer nome, ali é o nome da variável, vc pode modificar, pois se trata de uma instância.. lemnbra que aqui não é o nome da pessoa, e sim o nome da variável, o endereço na memória ok
 
 pessoa1.Nome = "Filipe";
 pessoa1.Idade = 15;
